c4d7fb2ac995a12e5f169aaad70552b74134c7fa
ybeom94 <ybeom94@nate.com>
2022-08-18 21:48:10 +0900

    Setup
    Singleton, JsonUtil, DOTween

---
+++ Assets/02,Scripts/Utils/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/02,Scripts/Utils/JsonUtilityHelper.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+
+public class JsonUtilityHelper
+{
+    public static T[] FromJson<T>(string json)
+    {
+        string newJson = "";
+        if (json[0] == '{')
+        {
+            newJson = json;
+        }
+        else
+        {
+            newJson = "{ \"array\": " + json + "}";
+        }
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+
+    public static string ToJson<T>(T[] array)
+    {
+        Wrapper<T> wrapper = new Wrapper<T>();
+        wrapper.array = array;
+        return JsonUtility.ToJson(wrapper);
+    }
+
+    [Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
