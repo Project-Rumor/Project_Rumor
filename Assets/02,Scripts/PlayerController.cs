7545ddcc4439634a7cfef096606442a66067ea5f
Kim Hyeonwoo <1reds@naver.com>
2022-08-20 01:28:37 +0900

    [Add] PlayerMove

---
+++ Assets/02,Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    float moveSpeed = 10.0f;
+    float hAxis = 0.0f;
+    float vAxis = 0.0f;
+
+    public PhotonView PV;
+    public SpriteRenderer SR;
+
+    void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if(PV.IsMine)
+        {
+            hAxis = Input.GetAxisRaw("Horizontal");
+            vAxis = Input.GetAxisRaw("Vertical");
+
+            transform.Translate(new Vector3(hAxis * Time.deltaTime * moveSpeed, vAxis * Time.deltaTime * moveSpeed, 0));
+
+            if (hAxis != 0)
+            {
+                PV.RPC("FilpXRPC", RpcTarget.AllBuffered, hAxis);
+            }
+        }
+    }
+
+    [PunRPC]
+    void FilpXRPC(float axis)
+    {
+        SR.flipX = (axis == -1);
+    }
+}
---
+++ Assets/02,Scripts/PlayerSkill.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerSkill : MonoBehaviourPunCallbacks
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
+
+
+}
