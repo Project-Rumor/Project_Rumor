381f59aa84718cfeff8be861cd83d6b4014fca02
SKW <rjsdntlsdj@gmail.com>
2022-08-21 01:07:40 +0900

    [Add] Ability

---
+++ Assets/02.Scripts/Characters/Ability/Ability.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+    string abilityName = "";
+    string abilityDescription = "";
+    float abilityCoolTime = 0.0f;
+
+    public CharacterCtrl CC;
+
+    public void Start()
+    {
+        CC = GetComponent<CharacterCtrl>();
+        abilityName = CC.chardata.abilityName;
+        abilityDescription = CC.chardata.ablityDescription;
+        abilityCoolTime = CC.chardata.ability;
+    }
+    public virtual void Active()
+    {
+
+    }
+}
---
+++ Assets/02.Scripts/Characters/Ability/ReaperAbility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperAbility : Ability
+{
+    public override void Active()
+    {
+        CC.moveSpeed += CC.chardata.skillvalue1;
+        // 시야
+        CC.attackRange += CC.chardata.skillvalue3;
+    }
+}
