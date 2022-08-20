d7884767a4cd80c393b6a8bfc8e876f204ca4542
SKW <rjsdntlsdj@gmail.com>
2022-08-20 23:26:03 +0900

    [Add] SoundManaager

---
+++ Assets/02.Scripts/Utils/SoundManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+}
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    [SerializeField] Sound[] sfx = null;
+    [SerializeField] Sound[] bgm = null;
+
+    [SerializeField] AudioSource bgmPlayer = null;
+    [SerializeField] AudioSource[] sfxPlayer = null;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlayBGM(string bgmName)
+    {
+        for (int i = 0; i < bgm.Length; i++)
+        {
+            if (bgmName == bgm[i].name)
+            {
+                bgmPlayer.clip = bgm[i].clip;
+                bgmPlayer.Play();
+            }
+        }
+    }
+
+    public void StopBGM()
+    {
+        bgmPlayer.Stop();
+    }
+
+    public void PlaySFX(string sfxName)
+    {
+        for (int i = 0; i < sfx.Length; i++)
+        {
+            if (sfxName == sfx[i].name)
+            {
+                for (int j = 0; j < sfxPlayer.Length; j++)
+                {
+                    if (!sfxPlayer[j].isPlaying)
+                    {
+                        sfxPlayer[j].clip = sfx[i].clip;
+                        sfxPlayer[j].Play();
+                        return;
+                    }
+                }
+                return;
+            }
+        }
+        return;
+    }
+}
